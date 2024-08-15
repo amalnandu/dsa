e62386a4bb841f77145cd9244fd644425ca73c0f
amalganesh <amalnanduwork@gmail.com>
2024-08-15 08:31:50 +0530

    day4

--- Arrays/mergeSortedArrays.cpp
+++ Arrays/mergeSortedArrays.cpp
@@ -1,6 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
+
+
+
+void merge(int arr1[],int n1, int arr2[],int n2 ){
+    int left = n1-1;
+    int right = 0;
+
+    while(left >=0 && right < n2 ){
+        if(arr1[left] >= arr2[right]){
+            int temp = arr1[left];
+            arr1[left] = arr2[right];
+            arr2[right] = temp;
+        }
+
+        left--;  right++;
+    }
+
+    sort(arr1,arr1+n1);
+    sort(arr2,arr2+n2);
+
+
+}
+
+void PrintArray(int arr[],int n){
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+
+
 int main() {
  
 int arr1[]={2,5,6,8,33,4,56,9};
@@ -12,7 +44,10 @@ int n2=sizeof(arr2)/sizeof(arr2[0]);
 sort(arr1,arr1+n1);
 sort(arr2,arr2+n2);
 
-
+merge(arr1,n1,arr2,n2);
+PrintArray(arr1,n1);
+cout<<endl;
+PrintArray(arr2,n2);
 
 return 0;
 }
